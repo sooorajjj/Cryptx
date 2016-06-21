b49a1e63505f208db9457ebe268494143890fc74
sooorajjj <sooorajjj@gmail.com>
2016-06-21 14:13:41 +0530

    Lets Go:
        * Populating Layout With Received Data:Adding
        * Dynamically Loading NoteViewFragment

--- java/online/klok/filemanager/FileDetailActivity.java
+++ java/online/klok/filemanager/FileDetailActivity.java
@@ -1,5 +1,7 @@
 package online.klok.filemanager;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class FileDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_detail);
+
+        createAndFragment();
+    }
+
+    private void  createAndFragment(){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FileViewFragment fileViewFragment = new FileViewFragment();
+        setTitle(R.string.viewFragmentTitle);
+        fragmentTransaction.add(R.id.file_container, fileViewFragment, "NOTE_VIEW_FRAGMENT");
+
+        fragmentTransaction.commit();
+
     }
 }
--- java/online/klok/filemanager/FileViewFragment.java
+++ java/online/klok/filemanager/FileViewFragment.java
@@ -1,11 +1,14 @@
 package online.klok.filemanager;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -22,8 +25,26 @@ public class FileViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_file_view, container, false);
+
+        View fragmentLayout = inflater.inflate(R.layout.fragment_file_view, container, false);
+
+        TextView title = (TextView) fragmentLayout.findViewById(R.id.viewFileTitle);
+        TextView message = (TextView) fragmentLayout.findViewById(R.id.viewFileMessage);
+        ImageView icon = (ImageView) fragmentLayout.findViewById(R.id.viewFileIcon);
+
+        Intent intent = getActivity().getIntent();
+
+        title.setText(intent.getExtras().getString(MainActivity.FILE_TITLE_EXTRA));
+        message.setText(intent.getExtras().getString(MainActivity.FILE_MESSAGE_EXTRA));
+
+        File.Category filecat = (File.Category) intent.getSerializableExtra(MainActivity.FILE_CATEGORY_EXTRA);
+        icon.setImageResource(File.categoryToDrawable(filecat));
+
+
+
+        return fragmentLayout;
     }
 
 }
+
+
