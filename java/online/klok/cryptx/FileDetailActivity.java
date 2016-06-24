adddb360882398edca88a62e3788956166a352c3
sooorajjj <sooorajjj@gmail.com>
2016-06-24 17:05:53 +0530

    Dynamically Adding And Starting Noteeditfragment

--- java/online/klok/cryptx/FileDetailActivity.java
+++ java/online/klok/cryptx/FileDetailActivity.java
@@ -1,5 +1,6 @@
 package online.klok.cryptx;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +18,27 @@ public class FileDetailActivity extends AppCompatActivity {
 
     private void  createAndFragment(){
 
+        Intent intent = getIntent();
+        MainActivity.FragmentToLaunch fragmentToLaunch =
+                (MainActivity.FragmentToLaunch) intent.getSerializableExtra(MainActivity.FILE_FRAGMENT_TOLOAD_EXTRA);
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        FileViewFragment fileViewFragment = new FileViewFragment();
-        setTitle(R.string.view_fragment_title);
-        fragmentTransaction.add(R.id.file_container, fileViewFragment, "FILE_VIEW_FRAGMENT");
+        switch(fragmentToLaunch){
+            case EDIT:
+
+                FileEditFragment fileEditFragment= new FileEditFragment();
+                setTitle(R.string.edit_fragment_title);
+                fragmentTransaction.add(R.id.file_container, fileEditFragment, "FILE_EDIT_FRAGMENT");
+
+                break;
+            case VIEW:
+                FileViewFragment fileViewFragment = new FileViewFragment();
+                setTitle(R.string.view_fragment_title);
+                fragmentTransaction.add(R.id.file_container, fileViewFragment, "FILE_VIEW_FRAGMENT");
+                break;
+        }
 
         fragmentTransaction.commit();
 
